67894a3904e24860f4c0ce70512bfb9987e51e84
Martin Clausen <martin.clausen@gmail.com>
2012-03-07 22:11:35 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject qtest "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [quil "1.0.0-SNAPSHOT"]]
+  :main qtest.core)
